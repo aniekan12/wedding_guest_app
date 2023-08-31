90b063301d49dd3aa7a2ef2915679cfa8e02de89
ani <godwin@istrategytech.com>
2023-08-31 12:53:50 +0100

    add jvmdefault args

--- app/src/main/java/com/ani/weddingguestapp/data/view_models/home_view_model/factory/HomeViewModelFactory.kt
+++ app/src/main/java/com/ani/weddingguestapp/data/view_models/home_view_model/factory/HomeViewModelFactory.kt
@@ -6,7 +6,7 @@ import com.ani.weddingguestapp.data.view_models.home_view_model.HomeViewModel
 import com.ani.weddingguestapp.domain.repositories.home.HomeRepository
 
 class HomeViewModelFactory(private val repository: HomeRepository) :
-    ViewModelProvider.AndroidViewModelFactory() {
+    ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
             return HomeViewModel(repository = repository) as T
